1476a81ec2ced92bee0b2e170f04c534ad78b034
KhaledAlhamad <khaledalhamad997@gmail.com>
2021-11-14 16:48:56 +0300

    Added login to user route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,26 +7,22 @@ router.use(express.json());
 
 const fs = require("fs");
 
+// GET users 
 router.get("/", (req, res) => {
   fs.readFile("./db/user.json", "utf8", (err, data) => {
     res.send(data);
   });
 });
 
-router.post("/", async (req, res) => {
+// ADD new user SignUp
+router.post("/signup", async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
     fs.readFile("./db/user.json", "utf8", (err, data) => {
       let arr = JSON.parse(data);
-
-    //   console.log(arr)
-    //   console.log(salt);
-    //   console.log(hashedPassword);
-
-      const user = { email: req.body.email, password: hashedPassword };
-      arr.push(user);
+      const newUser = { email: req.body.email, password: hashedPassword };
+        arr.push(newUser);
       fs.writeFile("./db/user.json", JSON.stringify(arr), (err) => {
         res.send("added");
       });
@@ -35,5 +31,27 @@ router.post("/", async (req, res) => {
     res.status(500).send();
   }
 });
+
+// Login
+router.post("/login", async (req, res) => {
+      fs.readFile("./db/user.json", "utf8", (err, data) => {
+        let arr = JSON.parse(data);
+        const user = arr.find(user => req.body.email)
+        if(user == null){
+            return res.status(400).send("Cannot find user")
+        }
+      });
+      try{
+            if(await bcrypt.compare(req.body.password, user.password)){
+                res.send("Success")
+            }
+            else{
+                res.send("NOT Allowed")
+            }
+    }
+     catch {
+      res.status(500).send();
+    }
+  });
 
 module.exports = router;
