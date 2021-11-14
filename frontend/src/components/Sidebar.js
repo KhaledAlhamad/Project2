e2559680f7fa2e72cb9ac61c5787ccc4efdb8095
KhaledAlhamad <khaledalhamad997@gmail.com>
2021-11-14 18:28:46 +0300

    added signup form in Login.js

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import * as ReactBootStrap from "react-bootstrap";
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+// import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function Sidebar() {
