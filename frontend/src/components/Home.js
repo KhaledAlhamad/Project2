7ff5df81d1bea0469cc524c1dea6755748e9542a
KhaledAlhamad <khaledalhamad997@gmail.com>
2021-11-15 11:54:50 +0300

    changes to Home component. added styles to app.css

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,27 +1,77 @@
 import React from "react";
 // import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+// import font
 
 function Home(props) {
   return (
-
     // HERO SECTION
 
     // <div id="parent">
+
     <section className="product spad">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8">
-          <div className="row">
-          {props.names.map((e, i) => {
-            return (
-              <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="product__item">
-                  <div
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <div className="trending__product">
+              <div class="row">
+                <div class="col-lg-8 col-md-8 col-sm-8">
+                  <div class="section-title">
+                    <h4>Trending Now</h4>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                {props?.airing?.map((e, i) => {
+                  return (
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="product__item">
+                        <div
                           class="product__item__pic set-bg"
-                          style={{backgroundImage: "url(" + e.image_url + ")"}}
+                          style={{
+                            backgroundImage: "url(" + e.image_url + ")",
+                          }}
                         >
-                        
+                          <div class="ep">{e.episodes}</div>
+                          <div class="comment">
+                            <i class="fa fa-comments"></i> 11
+                          </div>
+                          <div class="view">
+                            <i class="fa fa-eye"></i> {e.score}/10
+                          </div>
+                        </div>
+                        <div class="product__item__text">
+                          {/* <ul>
+                            <li>Active</li>
+                            <li>Movie</li>
+                          </ul> */}
+                          <h5>
+                            <a>
+                              <Link to="/details">{e.title}</Link>{" "}
+                            </a>
+                          </h5>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+                <div class="row">
+                <div class="col-lg-8 col-md-8 col-sm-8">
+                  <div class="section-title">
+                    <h4>Top</h4>
+                  </div>
+                </div>
+              </div>
+                {props?.top?.map((e, i) => {
+                  return (
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="product__item">
+                        <div
+                          class="product__item__pic set-bg"
+                          style={{
+                            backgroundImage: "url(" + e.image_url + ")",
+                          }}
+                        >
                           <div class="ep">18 / 18</div>
                           <div class="comment">
                             <i class="fa fa-comments"></i> 11
@@ -30,32 +80,27 @@ function Home(props) {
                             <i class="fa fa-eye"></i> 9141
                           </div>
                         </div>
-                  {/* <img src={e.image_url} /> */}
-                  <div class="product__item__text">
-                    <ul>
-                      <li>Active</li>
-                      <li>Movie</li>
-                    </ul>
-                    <h5>
-                      <a href="#">{e.title}</a>
-                    </h5>
-                  </div>
-                </div>
+                        <div class="product__item__text">
+                          <ul>
+                            <li>Active</li>
+                            <li>Movie</li>
+                          </ul>
+                          <h5>
+                            <a href="#">{e.title}</a>
+                          </h5>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
-              // <div className={i==0 || i==6 || i==12 || i==18 || i==24?"item_left"
-              // : i==3 || i==9 || i==15 || i==21 ?"item_right":"item"}>
-              //   <h3>{e.title}</h3>
-              //   <img src={e.image_url} />
-              // </div>
-            );
-          })}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-     {/* </div> */}
+      {/* </div> */}
     </section>
-    // HERO SECTION END 
+    // HERO SECTION END
   );
 }
 
