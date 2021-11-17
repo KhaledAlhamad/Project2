de7a31523ba4aa0292f1565d93c62040b6caebde
KhaledAlhamad <khaledalhamad997@gmail.com>
2021-11-17 03:06:47 +0300

    added (Trending, Top, Season)components and some style adjustment

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -38,7 +38,7 @@ function Home(props) {
       <section className="product spad">
         <div className="container">
           <div className="row">
-            <div className="col-lg-8">
+            <div className="col-lg">
               <div className="trending__product">
                 <div className="row">
                   <div className="col-lg-8 col-md-8 col-sm-8">
@@ -50,45 +50,45 @@ function Home(props) {
                   <div class="col-lg-4 col-md-4 col-sm-4">
                     <div class="btn__all">
                       <a class="primary-btn">
-                        View All <span class="arrow_right"></span>
+                       <Link to='/Trending'>View All <span class="arrow_right"></span></Link> 
                       </a>
                     </div>
                   </div>
                 </div>
                 <div className="row">
-                  {props?.airing?.map((e, i) => {
+                  {props?.airing?.slice(0,6).map((e, i) => {
                     return (
-                      <div class="col-lg-4 col-md-6 col-sm-6">
-                        <div class="product__item">
-                          <div
-                            class="product__item__pic set-bg"
-                            style={{
-                              backgroundImage: "url(" + e.image_url + ")",
-                            }}
-                          >
-                            <div class="ep">{e.episodes}</div>
-                            <div class="comment">
-                              <i class="fa fa-comments"></i> 11
-                            </div>
-                            <div class="view">
-                              <i class="fa fa-eye"></i> {e.score}/10
-                            </div>
-                          </div>
-                          <div class="product__item__text">
-                            {/* <ul>
-                            <li>Active</li>
-                            <li>Movie</li>
-                          </ul> */}
-                            <h5>
-                              <a>
-                                <Link to={`/Details/${e.mal_id}`}>
-                                  {e.title}
-                                </Link>{" "}
-                              </a>
-                            </h5>
+                      <div class="col-lg-2 col-md-6 col-sm-6">
+                      <div class="product__item">
+                        <div
+                          class="product__item__pic set-bg"
+                          style={{
+                            backgroundImage: "url(" + e.image_url + ")",
+                          }}
+                        >
+                          {e?.episodes ?<div class="ep">{e.episodes}</div> : ""}
+                          {/* <div class="comment">
+                            <i class="fa fa-comments"></i> 11
+                          </div> */}
+                          <div class="view">
+                            <i class="fa fa-eye"></i> {e.score}/10
                           </div>
                         </div>
+                        <div class="product__item__text">
+                          {/* <ul>
+                          <li>Active</li>
+                          <li>Movie</li>
+                        </ul> */}
+                          <h5>
+                            <a>
+                              <Link to={`/Details/${e.mal_id}`}>
+                                {e.title}
+                              </Link>{" "}
+                            </a>
+                          </h5>
+                        </div>
                       </div>
+                    </div>
                     );
                   })}
                   <div class="row">
@@ -97,40 +97,99 @@ function Home(props) {
                         <h4>Top</h4>
                       </div>
                     </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4">
+                    <div class="btn__all">
+                      <a class="primary-btn">
+                       <Link to='/Top'>View All <span class="arrow_right"></span></Link> 
+                      </a>
+                    </div>
                   </div>
-                  {props?.top?.map((e, i) => {
+                  </div>
+                  
+                  {props?.top?.slice(0,6).map((e, i) => {
                     return (
-                      <div class="col-lg-4 col-md-6 col-sm-6">
-                        <div class="product__item">
-                          <div
-                            class="product__item__pic set-bg"
-                            style={{
-                              backgroundImage: "url(" + e.image_url + ")",
-                            }}
-                          >
-                            <div class="ep">18 / 18</div>
-                            <div class="comment">
-                              <i class="fa fa-comments"></i> 11
-                            </div>
-                            <div class="view">
-                              <i class="fa fa-eye"></i> 9141
-                            </div>
-                          </div>
-                          <div class="product__item__text">
-                            <ul>
-                              <li>Active</li>
-                              <li>Movie</li>
-                            </ul>
-                            <h5>
-                              <a>
-                                <Link to={`/Details/${e.mal_id}`}>
-                                  {e.title}
-                                </Link>{" "}
-                              </a>
-                            </h5>
+                      <div class="col-lg-2 col-md-6 col-sm-6">
+                      <div class="product__item">
+                        <div
+                          class="product__item__pic set-bg"
+                          style={{
+                            backgroundImage: "url(" + e.image_url + ")",
+                          }}
+                        >
+                          {e?.episodes ?<div class="ep">{e.episodes}</div> : ""}
+                          {/* <div class="comment">
+                            <i class="fa fa-comments"></i> 11
+                          </div> */}
+                          <div class="view">
+                            <i class="fa fa-eye"></i> {e.score}/10
                           </div>
                         </div>
+                        <div class="product__item__text">
+                          {/* <ul>
+                          <li>Active</li>
+                          <li>Movie</li>
+                        </ul> */}
+                          <h5>
+                            <a>
+                              <Link to={`/Details/${e.mal_id}`}>
+                                {e.title}
+                              </Link>{" "}
+                            </a>
+                          </h5>
+                        </div>
                       </div>
+                    </div>
+                    );
+                  })}
+                  <div class="row">
+                    <div class="col-lg-8 col-md-8 col-sm-8">
+                      <div class="section-title">
+                        <h4>Season</h4>
+                      </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4">
+                    <div class="btn__all">
+                      <a class="primary-btn">
+                       <Link to='/Season'>View All <span class="arrow_right"></span></Link> 
+                      </a>
+                    </div>
+                  </div>
+                  </div>
+                  
+                  {props?.season?.slice(0,6).map((e, i) => {
+                    console.log(e.title)
+                    return (
+                      <div class="col-lg-2 col-md-6 col-sm-6">
+                      <div class="product__item">
+                        <div
+                          class="product__item__pic set-bg"
+                          style={{
+                            backgroundImage: "url(" + e.image_url + ")",
+                          }}
+                        >
+                          {e?.episodes ?<div class="ep">{e.episodes}</div> : ""}
+                          {/* <div class="comment">
+                            <i class="fa fa-comments"></i> 11
+                          </div> */}
+                          <div class="view">
+                            <i class="fa fa-eye"></i> {e.score}/10
+                          </div>
+                        </div>
+                        <div class="product__item__text">
+                          {/* <ul>
+                          <li>Active</li>
+                          <li>Movie</li>
+                        </ul> */}
+                          <h5>
+                            <a>
+                              <Link to={`/Details/${e.mal_id}`}>
+                                {e.title}
+                              </Link>{" "}
+                            </a>
+                          </h5>
+                        </div>
+                      </div>
+                    </div>
                     );
                   })}
                 </div>
